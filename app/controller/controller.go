55f8643e9fc0a2dd5321b142107449f28d8bd77b
Ryan Murulo <rkruberg@gmail.com>
2025-10-08 17:59:08 +0300

    feat: require pg UUID, section off controller struct related things

--- app/controller/controller.go
+++ app/controller/controller.go
@@ -1,30 +1,28 @@
 package controller
 
 import (
+	"fmt"
 	"net/http"
 	"roommates/db/dbqueries"
 	"roommates/gintemplrenderer"
 	"roommates/logger"
 	"roommates/rdb"
 	"roommates/utils"
+	"strconv"
 
 	"github.com/a-h/templ"
 	"github.com/gin-gonic/gin"
+	"github.com/jackc/pgx/v5/pgtype"
 	"github.com/jackc/pgx/v5/pgxpool"
 )
 
 var log = logger.ControllerLoggger
 
-// logs the server error and responds with public error as message
-func HandleServerError(ctx *gin.Context, err error, publicError string) {
-	// TODO: dump request into a file for easier reproducibility
-	log.Error().Err(err).Caller().Msg(publicError)
-	utils.ServerErrorResponse(ctx, publicError)
-}
-
 type SimpleResponse struct {
 	Message string `json:"message" example:"OK"`
 }
+
+// ---| CONTROLLER ---
 
 type Controller struct {
 	DB   *dbqueries.Queries
@@ -49,7 +47,35 @@ func (c *Controller) GetRH() *rdb.RedisHandler {
 	return c.RH
 }
 
+// --- CONTROLLER |---
+
 func RenderTempl(ctx *gin.Context, component templ.Component) {
 	r := gintemplrenderer.New(ctx.Request.Context(), http.StatusOK, component)
 	ctx.Render(r.Status, r)
+}
+
+// logs the server error and responds with public error as message
+func HandleServerError(ctx *gin.Context, err error, publicError string) {
+	// TODO: dump request into a file for easier reproducibility
+	log.Error().Err(err).Caller(1).Msg(publicError)
+	utils.ServerErrorResponse(ctx, publicError)
+}
+
+// will get UUID from uri (:id)
+//
+// will also write a response to when UUID is not valid,
+// return will be nil when that occurs
+//
+// this function only exists since binding pgtype.UUID is problematic, text marshal no bueno :(
+func requirePgUUID(ctx *gin.Context, param string) *pgtype.UUID {
+	reqParam := ctx.Param(param)
+	var id pgtype.UUID
+	id.Scan(reqParam)
+
+	if !id.Valid {
+		err := fmt.Errorf("invalid id (%s) in the url param", strconv.Quote(param))
+		utils.ErrorResponse(ctx, http.StatusForbidden, err)
+		return nil
+	}
+	return &id
 }
